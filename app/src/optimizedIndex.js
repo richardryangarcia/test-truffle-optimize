aa46188bde875fbc5d8cd63f731d6d36ba72e25a
Richard Ryan Garcia <garcia.richard.b@heb.com>
2019-09-18 20:26:42 -0500

    simple truffle project to test optimize plugin

---
+++ app/src/optimizedIndex.js
@@ -0,0 +1,5 @@
+export const setCompleted = "setCompleted_u1Q";
+export const upgrade = "upgrade_Ad$";
+export const set = "set_W2u";
+export const setStuff = "setStuff_c2K";
+export const setSetStuff = "setSetStuff_htL";
